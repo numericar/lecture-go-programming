c33d0efa83e2afa6f027c3f10c52b34b59222a95
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-04-13 16:08:31 +0700

    input

--- codes/investment_calculator.go
+++ codes/investment_calculator.go
@@ -6,11 +6,17 @@ import (
 )
 
 func calculate() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate float64 = 2.5
+	var investmentAmount float64
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	var futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
